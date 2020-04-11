475b33952c361110cad2fd5611fd65fc1ae4fc6f
Vishnu <vishnusr01@gmail.com>
2020-04-11 16:48:31 +0800

    Initial commit

---
+++ src/test/java/com/demo/graph/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
